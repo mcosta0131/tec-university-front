17c8caf7a448e047be87651e21faadb8add62aa7
MATEUS COSTA DOS SANTOS <mcosta0131@gmail.com>
2019-08-28 14:42:41 -0300

    Inicio desenvovimento de login

--- src/application/views/login/login.view.ts
+++ src/application/views/login/login.view.ts
@@ -1,17 +1,17 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 
-@Component({
-    selector: 'tec-app-login',
-    templateUrl: 'login.view.html'
-})
 
-
+@Component({selector: 'tec-app-login', templateUrl: 'login.view.html', styleUrls: ['./login.view.scss']})
 export class LoginView {
 
-    
+    form:       FormGroup;
 
     ngOnInit() {
-        
+        this.form = new FormGroup({
+            usuario: new FormControl('', Validators.required),
+            senha: new FormControl('',   Validators.required)
+        })
     }
 }
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppView } from 'src/application/views/app/app.view';
+import { LoginView } from 'src/application/views/login/login.view';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppView
+    AppView,
+    LoginView,
+    
   ],
   imports: [
     BrowserModule,
-    
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppView]
+  bootstrap: [LoginView]
 })
 export class AppModule { }
